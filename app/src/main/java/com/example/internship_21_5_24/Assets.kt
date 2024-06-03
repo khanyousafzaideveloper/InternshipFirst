a011ea90245557473b01f2ffe0ab80a5736e0f43
Ahmad <khanyousafzaideveloper@gmail.com>
2024-06-03 18:48:01 +0500

    WebView

--- app/src/main/java/com/example/internship_21_5_24/Assets.kt
+++ app/src/main/java/com/example/internship_21_5_24/Assets.kt
@@ -6,6 +6,8 @@ import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
 
@@ -16,6 +18,8 @@ fun Assets(){
         factory = { context ->
             WebView(context).apply {
                 settings.javaScriptEnabled = true
+                settings.defaultFontSize = 16
+                settings.loadsImagesAutomatically = true
                 settings.loadWithOverviewMode = true
                 settings.useWideViewPort = true
                 webViewClient = WebViewClient()
@@ -23,7 +27,7 @@ fun Assets(){
         },
         modifier = Modifier.fillMaxSize(),
         update = { webView ->
-            webView.loadUrl("file:///android_asset/Bypass.html")
+            webView.loadUrl("file:///android_asset/tool_lab.html")
         }
     )
 }
